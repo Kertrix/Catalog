3983e625b9106765c7741133bea0f50419ead028
Tristan Kermorvant <tristan.kermorvant@gmail.com>
2024-04-10 22:32:43 +0200

    🔨 Added script and pre-commit

---
+++ generate_json.py
@@ -0,0 +1,40 @@
+import hashlib
+import json
+import math
+import os
+
+import yaml
+
+
+def hash_name(name):
+    hsh = hashlib.sha256(name.encode())
+    return hsh.hexdigest()[:8]
+
+
+def convert_yaml_to_json(yaml_file):
+    with open(yaml_file, "r") as file:
+        data = yaml.safe_load(file)
+    return data
+
+
+def process_folder():
+    all_data = []
+    for root, dirs, files in os.walk("catalog"):
+        for file in files:
+            if file.endswith(".yaml") or file.endswith(".yml"):
+                yaml_file = os.path.join(root, file)
+                raw_data = convert_yaml_to_json(yaml_file)
+                _id = hash_name(raw_data["name"])
+
+                for key, value in raw_data.items():
+                    if isinstance(value, float) and math.isnan(value):
+                        raw_data[key] = ""
+
+                data = {"id": _id, **raw_data}
+                all_data.append(data)
+
+    with open("catalog.json", "w") as file:
+        json.dump(all_data, file, indent=2)
+
+
+process_folder()
