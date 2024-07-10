bf6a31956e2ef12ed828a1880de4a7d10315f25a
Tristan Kermorvant <tristan.kermorvant@gmail.com>
2024-07-10 20:07:49 +0200

    🔧  Added CORS

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require("path");
 const app = express();
 const port = 8000;
 
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.header("Content-Type", "application/json");
   res.sendFile(path.join(__dirname, "catalog.json"));
