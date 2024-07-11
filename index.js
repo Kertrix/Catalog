6c67a63f6e5a627ac1d305fd0589ab35bc9a5ca2
Tristan Kermorvant <tristan.kermorvant@gmail.com>
2024-07-11 11:17:25 +0200

    ✨  Add YAML and Bib file endpoints for item details

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
+const yaml = require("js-yaml");
+const fs = require("fs");
 
 const app = express();
 const port = 8000;
@@ -9,6 +12,37 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.header("Content-Type", "application/json");
   res.sendFile(path.join(__dirname, "catalog.json"));
+});
+
+app.get("/detail/:id", (req, res) => {
+  // Send the YAML file for the requested item (in JSON)
+  const id = req.params.id;
+  try {
+    const doc = yaml.load(
+      fs.readFileSync(
+        path.join(__dirname, "catalog", `${id}/${id}.yaml`),
+        "utf8"
+      )
+    );
+    res.json(doc);
+  } catch (e) {
+    res.status(404).send("Not found");
+  }
+});
+
+app.get("/detail/:id/bib", (req, res) => {
+  // Send the bib file for the requested item
+  const id = req.params.id;
+  try {
+    const bibFileContent = fs.readFileSync(
+      path.join(__dirname, "catalog", `${id}/${id}.bib`),
+      "utf8"
+    );
+    res.set("Content-Type", "text/plain");
+    res.send(bibFileContent);
+  } catch (e) {
+    res.status(404).send("Not found");
+  }
 });
 
 app.listen(port, () => {
